2c65d9b5a1593fbc5f5fbd927c89d6e5fb4337d0
yessinek97 <y.khanfir@instadeep.com>
2024-10-14 17:18:06 +0100

    fix conda env

--- check_data/test_data.py
+++ check_data/test_data.py
@@ -105,13 +105,13 @@ def test_kolmogorov_smirnov(data, ks_alpha):
     ]
 
     # Bonferroni correction for multiple hypothesis testing
-    # (see my blog post on this topic to see where this comes from:
+    # see Giacomo Vianello's (Auhtor of this course) blog post on this topic to see where this comes from:
     # https://towardsdatascience.com/precision-and-recall-trade-off-and-multiple-hypothesis-testing-family-wise-error-rate-vs-false-71a85057ca2b)
     alpha_prime = 1 - (1 - ks_alpha)**(1 / len(columns))
 
     for col in columns:
 
-        ts, p_value = scipy.stats.ks_2samp(sample1[col], sample2[col])
+        _, p_value = scipy.stats.ks_2samp(sample1[col].dropna(), sample2[col].dropna())
 
         # NOTE: as always, the p-value should be interpreted as the probability of
         # obtaining a test statistic (TS) equal or more extreme that the one we got
--- evaluate/run.py
+++ evaluate/run.py
@@ -6,7 +6,7 @@ import pandas as pd
 import wandb
 import mlflow.sklearn
 import matplotlib.pyplot as plt
-from sklearn.metrics import roc_auc_score, plot_confusion_matrix
+from sklearn.metrics import roc_auc_score
 
 logging.basicConfig(level=logging.INFO, format="%(asctime)-15s %(message)s")
 logger = logging.getLogger()
@@ -39,23 +39,6 @@ def go(args):
     run.summary["AUC"] = score
 
     logger.info("Computing confusion matrix")
-    fig_cm, sub_cm = plt.subplots(figsize=(10, 10))
-    plot_confusion_matrix(
-        pipe,
-        X_test[used_columns],
-        y_test,
-        ax=sub_cm,
-        normalize="true",
-        values_format=".1f",
-        xticks_rotation=90,
-    )
-    fig_cm.tight_layout()
-
-    run.log(
-        {
-            "confusion_matrix": wandb.Image(fig_cm)
-        }
-    )
 
 
 if __name__ == "__main__":
